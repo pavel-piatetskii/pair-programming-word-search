bef3e642980be30460059b3a42d1490dc0fcf8d7
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2020-11-05 04:06:44 +0000

    Completed the task without stretch

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -20,7 +20,7 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present horizontally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -34,5 +34,27 @@ describe("#wordSearch()", function() {
     ], 'SEINFELD')
 
     assert.isTrue(result);
+  });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'A', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'D', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'V', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'E', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'N', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'T', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'U', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'R', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'E', 'Q', 'U', 'A', 'L'],
+    ], 'ADVENTURE')
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the array is empty", function() {
+    const result = wordSearch([], 'ADVENTURE')
+
+    assert.isFalse(result);
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,34 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    if (letters.length === 0) return false;
+    const transposed = transpose(letters);  
+    if (!joinAndCheck(letters, word) && !joinAndCheck(transposed, word)) {
+        return false;
+    } else {
+        return true;
     }
 }
 
+const joinAndCheck = function(letters, word) {
+    const horizontalJoin = letters.map(ls => ls.join(''))
+    for (l of horizontalJoin) {
+        if (l.includes(word)) return true    
+    }
+    return false;
+}
+
 module.exports = wordSearch
+
+//Transpose Function
+const transpose = function(matrix) {
+    // Put your solution here
+    for (let i= 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix.length; j ++) {
+        let tmp = matrix[i][j];
+        matrix[i][j] = matrix[j][i];
+        matrix[j][i] = tmp;
+      }
+    }
+   return matrix[0].map((col, i) => matrix.map(row => row[i]))
+  };
+  
+ 
